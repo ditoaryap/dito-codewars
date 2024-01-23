09e701e25526c00bceadcae56b3c3cc7cb3d6d92
Dito Aryaputra Ramadhani <ditoaryaputra1324@gmail.com>
2024-01-23 13:54:55 +0700

    codewars - math logs

---
+++ 2-math-logs.js
@@ -0,0 +1,10 @@
+// Code Wars - Math Logs
+// https://www.codewars.com/kata/5b68c7029756802aa2000176/javascript
+
+// function logs(x, a, b) {
+//     return (Math.log(b) + Math.log(a)) / Math.log(x)
+// }
+
+let logs = (x, a, b) => result = (Math.log(b) + Math.log(a)) / Math.log(x)
+
+console.log(logs(1000, 2, 3))
