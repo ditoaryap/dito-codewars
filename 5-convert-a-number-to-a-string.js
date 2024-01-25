a155287b4eb5eb552431e83aff2a2fe732224340
Dito Aryaputra Ramadhani <ditoaryaputra1324@gmail.com>
2024-01-25 21:23:43 +0700

    convert a number to a string

---
+++ 5-convert-a-number-to-a-string.js
@@ -0,0 +1,9 @@
+// Code Wars - Convert a Number to a String!
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+
+function numberToString(num) {
+    let string = num.toString()
+    return string
+}
+
+console.log(numberToString(100))
