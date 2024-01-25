e0c0264bf24e186e8f25928f1582a31be14f90fb
Dito Aryaputra Ramadhani <ditoaryaputra1324@gmail.com>
2024-01-25 21:34:24 +0700

    sentence smash

---
+++ 6-Sentence Smash.js
@@ -0,0 +1,18 @@
+// Code Wars - Sentence Smash
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d
+
+
+// Solution 1
+// function smash(words) {
+//     words = words.join(' ');
+//     return words
+// };
+
+// Solution 2
+smash = function(words) {
+    smash.join(' ')
+}
+
+
+
+// console.log(smash(['hello', 'world', 'this', 'is', 'great']))
