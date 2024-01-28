796452c4bee204f4b652402164e141d8a15c3bb0
Dito Aryaputra Ramadhani <ditoaryaputra1324@gmail.com>
2024-01-28 19:52:59 +0700

    lost witout a map

---
+++ 11-lost without a map.js
@@ -0,0 +1,21 @@
+// Given an array of integers, return a new array with each value doubled.
+// For example:
+// [1, 2, 3] --> [2, 4, 6]
+
+
+// Answer 1
+// function maps(x) {
+//     return x.map(x => x + x);
+// }
+
+// console.log(maps([1, 2, 3]));
+
+// Answer 2
+function maps(x) {
+    let k = []
+    for (let i = 0; i < x.length; i++) {
+        k.push(x[i] + x[i])
+    }
+    return k
+}
+console.log(maps([1, 2, 3]))
