0ebfca586f388e801b1f8c19a7ba6d546446c8fd
Dito Aryaputra Ramadhani <ditoaryaputra1324@gmail.com>
2024-01-29 15:06:08 +0700

    update

--- 16-Get the Middle Character.js
+++ 16-Get the Middle Character.js
@@ -1,12 +1,20 @@
 // Code Wars - Get the Middle Character
 // https://www.codewars.com/kata/56747fd5cb988479af000028
 
+// Answer 1
+// function getMiddle(s) {
+//     a = s.length
+//     if (s.length % 2 === 0) {
+//         return s.slice(a / 2 - 1, a / 2 + 1)
+//     } else {
+//         return s.slice(a / 2, a / 2 + 1)
+//     }
+// }
+
+// Answer 2
 function getMiddle(s) {
-    a = s.length
-    if (s.length % 2 === 0) {
-        return s.slice(a / 2 - 1, a / 2 + 1)
-    } else {
-        return s.slice(a / 2, a / 2 + 1)
-    }
+    return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
 }
+
+
 console.log(getMiddle('testing'))
