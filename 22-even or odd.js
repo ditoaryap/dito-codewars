e6430846cc741bf6fd41523a4bbc02c46ae2e650
Dito Aryaputra Ramadhani <ditoaryaputra1324@gmail.com>
2024-01-30 20:58:01 +0700

    code wars - even or odd (8 kyu)

---
+++ 22-even or odd.js
@@ -0,0 +1,12 @@
+// Code Wars - Even or Odd (8 Kyu)
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+function evenOrOdd(number) {
+    if (number % 2 === 0) {
+        return "Even"
+    } else if (number % 2 === 1 || number % 2 === -1) {
+        return "Odd"
+    }
+}
+
+console.log(evenOrOdd(-5));
