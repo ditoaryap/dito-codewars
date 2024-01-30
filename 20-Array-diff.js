1a80bdabaf7ae2f557560ee4b7d5d0056f333ce4
Dito Aryaputra Ramadhani <ditoaryaputra1324@gmail.com>
2024-01-30 15:13:32 +0700

    Code Wars - Array.diff (6 Kyu)

---
+++ 20-Array-diff.js
@@ -0,0 +1,8 @@
+// Code Wars - Array.diff (6 Kyu)
+// https://www.codewars.com/kata/523f5d21c841566fde000009
+
+function arrayDiff(a, b) {
+    return a.filter(a => !b.includes(a));
+}
+
+console.log(arrayDiff([1, 2, 3], [1, 2]))
