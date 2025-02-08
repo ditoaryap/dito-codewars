330612945de3ca7bf2fb17afbdcb73cead70b441
Dito Aryaputra Ramadhani <ditoaryaputra1324@gmail.com>
2025-02-09 02:50:12 +0700

    update

---
+++ 24-remove-frist-and-last-character.js
@@ -0,0 +1,16 @@
+// example
+
+// assert.strictEqual(removeChar('eloquent'), 'loquen');
+// assert.strictEqual(removeChar('country'), 'ountr');
+// assert.strictEqual(removeChar('person'), 'erso');
+// assert.strictEqual(removeChar('place'), 'lac');
+// assert.strictEqual(removeChar('ab'), '')
+// assert.strictEqual(removeChar('ooopsss'), 'oopss');
+
+
+function removeChar(str) {
+    let test = "eloquent"
+    return test.slice(1)
+}
+
+console.log('test')
