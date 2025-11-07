81066896f75713db2e5dd7559c3d09aaf582c065
ditoaryap <ditoaryaputra1324@gmail.com>
2025-11-07 15:08:08 +0700

    add 2 folder js & php

--- 30-Are You Playing Banjo.js
+++
@@ -1,9 +0,0 @@
-function areYouPlayingBanjo(name) {
-  if(name.substring(0) = )
-  return name;
-}
-
-console.log("test");
-
-let a = "dito";
-console.log(a.substring(0, 1));
---
+++ JavaScript/30-Are You Playing Banjo.js
@@ -0,0 +1,14 @@
+// function areYouPlayingBanjo(name) {
+//   if(name.substring(0) = )
+//   return name;
+// }
+
+// console.log("test");
+
+// let a = "dito";
+// console.log(a.substring(0, 1));
+
+let a = 4350000;
+let b = 26
+
+console.log(a * b);
