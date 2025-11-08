4cbe082fe921af7f2c9033e215a793a08464df8a
ditoaryap <ditoaryaputra1324@gmail.com>
2025-11-08 21:40:24 +0700

    7

---
+++ PHP/7. Calculate average.php
@@ -0,0 +1,17 @@
+<?php
+
+// function find_average($array): float
+// {
+//     $a = count($array);
+//     return $array / $a;
+// }
+
+// echo find_average([1, 2, 3]);
+
+$b = [1, 2, 3, 4];
+$c = array_sum($b);
+// $d = ...$b;
+
+echo $c;
+
+// var_dump(count($b) / $b);
