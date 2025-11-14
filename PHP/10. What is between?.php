0150a881bfcef19fb341f7ecc629ce4afc1d7c88
ditoaryap <ditoaryaputra1324@gmail.com>
2025-11-14 21:52:41 +0700

    10. What is between? CARA KE 2

--- PHP/10. What is between?.php
+++ PHP/10. What is between?.php
@@ -1,12 +1,19 @@
 <?php
 
+// Cara 1
+// function between(int $a, int $b): array
+// {
+//     $c = [];
+//     for ($i = $a; $i <= $b; $i++) {
+//         $c[] = $i;
+//     }
+//     return $c;
+// }
+
+// Cara 2
 function between(int $a, int $b): array
 {
-    $c = [];
-    for ($i = $a; $i <= $b; $i++) {
-        $c[] = $i;
-    }
-    return $c;
+    return range($a, $b);
 }
 
 print_r(between(1, 4));
